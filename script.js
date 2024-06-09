daa328c1499ee2441780a071c22f6781a6b65a32
joshhhhjoh <128190607+joshhhhjoh@users.noreply.github.com>
2024-06-09 16:49:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function e(){
+  navigator.clipboard.writeText("__.____._______.__._")
+
+  document.getElementById('s').style.backgroundImage = "url('wow.jpeg')"
+  document.getElementById('s').style.backgroundSize = "cover"
+
+  document.getElementById('s').style.backgroundRepeat = "no-repeat";
+}
